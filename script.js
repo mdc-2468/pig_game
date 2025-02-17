8c138a806c0712237f37b396cea76ea0e7b5b603
mdc-2468 <mandychan1019@outlook.com>
2025-02-17 14:05:39 +0800

    Handling for New Game and how to win the game

--- script.js
+++ script.js
@@ -10,12 +10,11 @@ let dice = 0;
 const diceImg = document.querySelector(".dice")
 const rollDiceBtn = document.querySelector(".btn--roll");
 const holdBtn = document.querySelector(".btn--hold");
+const newGameBtn = document.querySelector(".btn--new");
 const totalScore0UI = document.querySelector("#score--0");
 const totalScore1UI = document.querySelector("#score--1");
 const currentScore0UI = document.querySelector("#current--0");
 const currentScore1UI = document.querySelector("#current--1");
-
-
 
 
 function rollDice() {
@@ -46,6 +45,10 @@ function rollDice() {
 
 function updateCurrentPlayer() {
   currentPlayer === 0 ? currentPlayer = 1 : currentPlayer = 0;
+
+  document.querySelector('.player--0').classList.toggle("player--active");
+  document.querySelector('.player--1').classList.toggle("player--active");
+
   console.log(`Now it's player ${currentPlayer}'s turn`);
 
 }
@@ -74,11 +77,11 @@ function addCurrent(diceValue, player) {
       currentScore1UI.textContent = currentScore1;
 
     }
+
     // Update Current Player
     updateCurrentPlayer();
   }
 }
-
 
 function hold(player) {
   console.log(`Player ${player} clicked hold`);
@@ -86,29 +89,55 @@ function hold(player) {
   if (player === 0) {
     console.log(`Player ${currentPlayer} added ${currentScore0} to total`);
     totalScore0 += currentScore0
-    currentScore0 = 0
-    console.log(`Player ${currentPlayer} has now total ${totalScore0}`);
     totalScore0UI.textContent = totalScore0;
+    console.log(`Player ${currentPlayer} has now total ${totalScore0}`);
+    currentScore0 = 0
     currentScore0UI.textContent = currentScore0;
+
+
 
   } else if (player === 1) {
     console.log(`Player ${currentPlayer} added ${currentScore1} to total`);
     totalScore1 += currentScore1;
-    currentScore1 = 0;
-    console.log(`Player ${currentPlayer} has now total ${totalScore1}`);
     totalScore1UI.textContent = totalScore1;
+    console.log(`Player ${currentPlayer} has now total ${totalScore1}`);
+    currentScore1 = 0;
     currentScore1UI.textContent = currentScore1;
   }
+
+  // Check > 100
+  setTimeout(() => {
+    if (totalScore0 >= 20) alert("Player 1 wins");
+    if (totalScore1 >= 20) alert("Player 2 wins");
+  }, 100); // Delay by 100ms to allow UI updates
 
 
   // Update Current Player
   updateCurrentPlayer();
 }
 
+function newGame() {
+  console.log("Staring new game...");
+  currentPlayer = 0;
+  currentScore0 = 0;
+  currentScore1 = 0;
+  totalScore0 = 0;
+  totalScore1 = 0;
+  dice = 0;
+
+  document.querySelector('.player--0').classList.add("player--active");
+  document.querySelector('.player--1').classList.remove("player--active");
+  currentScore0UI.textContent = String(currentScore0);
+  currentScore1UI.textContent = String(currentScore1);
+  totalScore0UI.textContent = String(totalScore0);
+  totalScore1UI.textContent = String(totalScore1);
+
+
+}
 
 // Fresh Start
 console.log("New Game Start...");
-console.log(`Let's start from: ${currentPlayer}`);
+console.log(`Let's start from: Player ${currentPlayer}`);
 
 // Start from 0
 totalScore0UI.textContent = totalScore0;
@@ -124,6 +153,9 @@ rollDiceBtn.addEventListener("click", function () {
   console.log(`Player ${currentPlayer} rolls a ${diceValue}`);
   addCurrent(diceValue, currentPlayer);
 });
+
 holdBtn.addEventListener("click", function () {
   hold(currentPlayer);
-}); 
+});
+
+newGameBtn.addEventListener("click", newGame);
