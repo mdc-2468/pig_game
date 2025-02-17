e829f2e6b3c3e52ee005909f59b6a64f68e39f6c
mdc-2468 <mandychan1019@outlook.com>
2025-02-18 01:07:36 +0800

    Refactoring the Functions

--- script.js
+++ script.js
@@ -1,50 +1,31 @@
 'use strict';
 
 let currentPlayer = 0;
-let currentScore0 = 0;
-let currentScore1 = 0;
-let totalScore0 = 0;
-let totalScore1 = 0;
+let currentScores = [0, 0];
+let totalScores = [0, 0];
 let dice = 0;
 
 const diceImg = document.querySelector(".dice")
 const rollDiceBtn = document.querySelector(".btn--roll");
 const holdBtn = document.querySelector(".btn--hold");
 const newGameBtn = document.querySelector(".btn--new");
-const totalScore0UI = document.querySelector("#score--0");
-const totalScore1UI = document.querySelector("#score--1");
-const currentScore0UI = document.querySelector("#current--0");
-const currentScore1UI = document.querySelector("#current--1");
+const totalScoresUI = [document.querySelector("#score--0"), document.querySelector("#score--1")];
+const currentScoresUI = [document.querySelector("#current--0"), document.querySelector("#current--1")];
 
 
 function rollDice() {
   dice = Math.floor(Math.random() * 6 + 1);
-  // console.log(dice);
-  switch (dice) {
-    case 1:
-      diceImg.setAttribute("src", "dice-1.png");
-      break;
-    case 2:
-      diceImg.setAttribute("src", "dice-2.png");
-      break;
-    case 3:
-      diceImg.setAttribute("src", "dice-3.png");
-      break;
-    case 4:
-      diceImg.setAttribute("src", "dice-4.png");
-      break;
-    case 5:
-      diceImg.setAttribute("src", "dice-5.png");
-      break;
-    case 6:
-      diceImg.setAttribute("src", "dice-6.png");
-      break;
-  }
+
+  diceImg.src = `dice-${dice}.png`;
+  diceImg.style.display = "Block";
+
+  console.log(`Player ${currentPlayer} rolls a ${dice}`);
   return dice;
+
 }
 
 function updateCurrentPlayer() {
-  currentPlayer === 0 ? currentPlayer = 1 : currentPlayer = 0;
+  currentPlayer = 1 - currentPlayer;
 
   document.querySelector('.player--0').classList.toggle("player--active");
   document.querySelector('.player--1').classList.toggle("player--active");
@@ -56,27 +37,13 @@ function updateCurrentPlayer() {
 
 function addCurrent(diceValue, player) {
   if (diceValue !== 1) {
-    if (player === 0) {
-      currentScore0 += diceValue;
-      currentScore0UI.textContent = currentScore0;
-
-    } else if (player === 1) {
-      currentScore1 += diceValue;
-      currentScore1UI.textContent = currentScore1;
-
-    }
+    currentScores[player] += diceValue;
+    currentScoresUI[player].textContent = currentScores[player];
 
   } else {
     console.log("Switch User because of dice = 1");
-    if (player === 0) {
-      currentScore0 = 0;
-      currentScore0UI.textContent = currentScore0;
-
-    } else if (player === 1) {
-      currentScore1 = 0;
-      currentScore1UI.textContent = currentScore1;
-
-    }
+    currentScores[player] = 0;
+    currentScoresUI[player].textContent = currentScores[player];
 
     // Update Current Player
     updateCurrentPlayer();
@@ -86,54 +53,37 @@ function addCurrent(diceValue, player) {
 function hold(player) {
   console.log(`Player ${player} clicked hold`);
 
-  if (player === 0) {
-    console.log(`Player ${currentPlayer} added ${currentScore0} to total`);
-    totalScore0 += currentScore0
-    totalScore0UI.textContent = totalScore0;
-    console.log(`Player ${currentPlayer} has now total ${totalScore0}`);
-    currentScore0 = 0
-    currentScore0UI.textContent = currentScore0;
-
-
-
-  } else if (player === 1) {
-    console.log(`Player ${currentPlayer} added ${currentScore1} to total`);
-    totalScore1 += currentScore1;
-    totalScore1UI.textContent = totalScore1;
-    console.log(`Player ${currentPlayer} has now total ${totalScore1}`);
-    currentScore1 = 0;
-    currentScore1UI.textContent = currentScore1;
-  }
+  totalScores[player] += currentScores[player];
+  totalScoresUI[player].textContent = totalScores[player];
+  console.log(`Player ${player} has added ${currentScores[player]}, the total is now ${totalScores[player]}`);
+  currentScores[player] = 0;
+  currentScoresUI[player].textContent = currentScores[player];
 
   // Check > 20
   setTimeout(() => {
-    if (totalScore0 >= 20) alert("Player 1 wins");
-    if (totalScore1 >= 20) alert("Player 2 wins");
-  }, 100); // Delay by 100ms to allow UI updates
-
+    if (totalScores[0] >= 20) alert("Player 1 wins");
+    if (totalScores[1] >= 20) alert("Player 2 wins");
+  }, 200); // Delay by 200ms to allow UI updates
 
   // Update Current Player
-  if (totalScore0 <= 20 && totalScore1 <= 20) updateCurrentPlayer();
+  if (totalScores[player] <= 20) updateCurrentPlayer();
 }
 
 function newGame() {
   console.log("Staring new game...");
   currentPlayer = 0;
-  currentScore0 = 0;
-  currentScore1 = 0;
-  totalScore0 = 0;
-  totalScore1 = 0;
+  currentScores = [0, 0];
+  totalScores = [0, 0];
   dice = 0;
 
   diceImg.style.display = "None";
   document.querySelector('.player--0').classList.add("player--active");
   document.querySelector('.player--1').classList.remove("player--active");
-  currentScore0UI.textContent = String(currentScore0);
-  currentScore1UI.textContent = String(currentScore1);
-  totalScore0UI.textContent = String(totalScore0);
-  totalScore1UI.textContent = String(totalScore1);
 
-
+  for (let i = 0; i < 2; i++) {
+    currentScoresUI[i].textContent = currentScores[i];
+    totalScoresUI[i].textContent = totalScores[i];
+  }
 }
 
 // Fresh Start
@@ -142,18 +92,13 @@ console.log(`Let's start from: Player ${currentPlayer}`);
 diceImg.style.display = "None";
 
 // Start from 0
-totalScore0UI.textContent = totalScore0;
-totalScore1UI.textContent = totalScore1;
-currentScore0UI.textContent = currentScore0;
-currentScore1UI.textContent = currentScore1;
-
-
+for (let i = 0; i < 2; i++) {
+  currentScoresUI[i].textContent = currentScores[i];
+  totalScoresUI[i].textContent = totalScores[i];
+}
 
 rollDiceBtn.addEventListener("click", function () {
-  // console.log(`Player ${currentPlayer} is rolling a dice...`);
-  let diceValue = rollDice();
-  diceImg.style.display = "Block";
-  console.log(`Player ${currentPlayer} rolls a ${diceValue}`);
+  let diceValue = rollDice()
   addCurrent(diceValue, currentPlayer);
 });
 
